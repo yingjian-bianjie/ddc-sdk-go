e04dfa4575a1ef593639bcd2d336e86ce8e7e5e9
ht <22350747@qq.com>
2022-05-26 18:19:25 +0800

    export AccountInfo fields

--- ddc-sdk-platform-go/app/models/dto/accountInfo.go
+++ ddc-sdk-platform-go/app/models/dto/accountInfo.go
@@ -10,6 +10,36 @@ type AccountInfo struct {
 	field         string
 }
 
+
 func NewAccountInfo(accDID string, accName string, accRole uint8, leaderDID string, platformState uint8, operatorState uint8, field string) *AccountInfo {
 	return &AccountInfo{accDID: accDID, accName: accName, accRole: accRole, leaderDID: leaderDID, platformState: platformState, operatorState: operatorState, field: field}
+}
+
+
+func (a *AccountInfo) SetAccDID(accDID string) {
+	a.accDID = accDID
+}
+
+func (a *AccountInfo) SetAccName(accName string) {
+	a.accName = accName
+}
+
+func (a *AccountInfo) SetAccRole(accRole uint8) {
+	a.accRole = accRole
+}
+
+func (a *AccountInfo) SetLeaderDID(leaderDID string) {
+	a.leaderDID = leaderDID
+}
+
+func (a *AccountInfo) SetPlatformState(platformState uint8) {
+	a.platformState = platformState
+}
+
+func (a *AccountInfo) SetOperatorState(operatorState uint8) {
+	a.operatorState = operatorState
+}
+
+func (a *AccountInfo) SetField(field string) {
+	a.field = field
 }
