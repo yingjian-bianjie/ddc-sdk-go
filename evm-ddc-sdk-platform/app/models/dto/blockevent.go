8a4e28f658dd6b298463bf6a43a40cce3af8c6cc
huangweichang <weichang@bianjie.ai>
2022-07-12 21:11:45 +0800

    add ddcs batch function

--- evm-ddc-sdk-platform/app/models/dto/blockevent.go
+++ evm-ddc-sdk-platform/app/models/dto/blockevent.go
@@ -1,6 +1,18 @@
 package dto
 
+import "github.com/ethereum/go-ethereum/core/types"
+
 type BlockEventBean struct {
 	Events    []interface{}
 	Timestamp string
+}
+
+type BlockDdcInfoBean struct {
+	DdcInfos  []DdcInfoBean
+	Timestamp string
+}
+
+type DdcInfoBean struct {
+	DdcId   uint64
+	Receipt *types.Receipt
 }
--- evm-ddc-sdk-platform/app/service/block.go
+++ evm-ddc-sdk-platform/app/service/block.go
@@ -67,6 +67,38 @@ func (b *BlockService) GetBlockEvents(blockNumber int64) (*dto.BlockEventBean, e
 	return &dto.BlockEventBean{Events: blockEvents, Timestamp: strconv.FormatUint(time, 10)}, nil
 }
 
+// GetBlockEvents
+// @Description: 获取指定区块内的所有ddcId和回执
+// @receiver b
+// @param blockNumber 块高
+// @return *dto.BlockDdcInfoBean ddc信息和时间戳的实体
+// @return error
+func (b *BlockService) GetBlockDdcs(blockNumber int64) (*dto.BlockDdcInfoBean, error) {
+
+	//查找区块中所有交易
+	block, err := config.Info.Conn().BlockByNumber(context.Background(), big.NewInt(blockNumber))
+	if err != nil {
+		log.Error.Printf("failed to execute BlockByNumber: %v", err.Error())
+		return nil, types2.NewSDKError(types2.QueryError.Error(), err.Error())
+	}
+	var blockDdcs []dto.DdcInfoBean
+	//查找每笔交易中的event
+	for _, tx := range block.Transactions() {
+		_, txReceipt, ddcId, err := b.GetTxEventsWithReceiptAndDdcId(tx.Hash())
+		if err != nil {
+			log.Error.Printf("failed to get events,receipt,ddcId of Tx: %v,error: %v", tx.Hash(), err.Error())
+			return nil, types2.NewSDKError(types2.QueryError.Error(), err.Error())
+		}
+		blockDdcs = append(blockDdcs, dto.DdcInfoBean{
+			Receipt: txReceipt,
+			DdcId:   ddcId,
+		})
+	}
+	time := block.Time()
+
+	return &dto.BlockDdcInfoBean{DdcInfos: blockDdcs, Timestamp: strconv.FormatUint(time, 10)}, nil
+}
+
 // GetTxEvents
 // @Description: 获取指定交易中的所有events
 // @receiver b
@@ -176,5 +208,119 @@ func (b *BlockService) GetTxEvents(txHash common.Hash) (events []interface{}, er
 		events = append(events, event)
 	}
 
+	return
+}
+
+// GetTxEventsWithReceipt
+// @Description: 获取指定交易中的所有events、回执信息、ddcID
+// @receiver b
+// @param txHash 交易哈希
+// @return events 查询出的事件
+// @return err
+func (b *BlockService) GetTxEventsWithReceiptAndDdcId(txHash common.Hash) (events []interface{},
+	txReceipt *types.Receipt, ddcId uint64, err error) {
+	//获取对应的交易回执
+	receipt, err := config.Info.Conn().TransactionReceipt(context.Background(), txHash)
+	if err != nil {
+		log.Error.Printf("failed to execute TransactionReceipt: %v", err.Error())
+		return nil, nil, 0, types2.NewSDKError(types2.QueryError.Error(), err.Error())
+	}
+	txReceipt = receipt
+	var event interface{}
+	//获取交易的logs中的所有log对应的event
+	for _, l := range receipt.Logs {
+		//1.匹配对应的合约
+		switch l.Address {
+		case config.Info.AuthorityAddress():
+			{
+				authority := handler.GetAuthority()
+				abi, err := contracts.AuthorityMetaData.GetAbi()
+				if err != nil {
+					log.Error.Printf("failed to get Authoriy abi: %v", err.Error())
+					return nil, txReceipt, 0, err
+				}
+				//2.匹配对应的事件
+				switch l.Topics[0] {
+				case abi.Events[constant.EventAddAccount].ID:
+					event, err = authority.ParseAddAccount(*l)
+				case abi.Events[constant.EventUpdateAccountState].ID:
+					event, err = authority.ParseUpdateAccountState(*l)
+				case abi.Events[constant.EventCrossPlatformApproval].ID:
+					event, err = authority.ParseCrossPlatformApproval(*l)
+				}
+			}
+		case config.Info.ChargeAddress():
+			{
+				charge := handler.GetCharge()
+				abi, err := contracts.ChargeMetaData.GetAbi()
+				if err != nil {
+					log.Error.Printf("failed to get Charge abi: %v", err.Error())
+					return nil, txReceipt, 0, err
+				}
+				switch l.Topics[0] {
+				case abi.Events[constant.EventRecharge].ID:
+					event, err = charge.ParseRecharge(*l)
+				case abi.Events[constant.EventPay].ID:
+					event, err = charge.ParsePay(*l)
+				case abi.Events[constant.EventSetFee].ID:
+					event, err = charge.ParseSetFee(*l)
+				case abi.Events[constant.EventDelFee].ID:
+					event, err = charge.ParseDelFee(*l)
+				case abi.Events[constant.EventDelDDC].ID:
+					event, err = charge.ParseDelDDC(*l)
+				}
+			}
+		case config.Info.Ddc721Address():
+			{
+				ddc721 := handler.GetDDC721()
+				abi, err := contracts.DDC721MetaData.GetAbi()
+				if err != nil {
+					log.Error.Printf("failed to get DDC721 abi: %v", err.Error())
+					return nil, txReceipt, 0, err
+				}
+				switch l.Topics[0] {
+				case abi.Events[constant.EventTransfer].ID:
+					event, err = ddc721.ParseTransfer(*l)
+				case abi.Events[constant.EventPay].ID:
+					event, err = ddc721.ParseOwnershipTransferred(*l)
+				case abi.Events[constant.EventEnterBlacklist].ID:
+					event, err = ddc721.ParseEnterBlacklist(*l)
+				case abi.Events[constant.EventExitBlacklist].ID:
+					event, err = ddc721.ParseExitBlacklist(*l)
+				case abi.Events[constant.EventSetURI].ID:
+					event, err = ddc721.ParseSetURI(*l)
+				}
+			}
+		case config.Info.Ddc1155Address():
+			{
+				ddc1155 := handler.GetDDC1155()
+				abi, err := contracts.DDC1155MetaData.GetAbi()
+				if err != nil {
+					log.Error.Printf("failed to get DDC1155 abi: %v", err.Error())
+					return nil, txReceipt, 0, err
+				}
+				switch l.Topics[0] {
+				case abi.Events[constant.EventTransferSingle].ID:
+					event, err = ddc1155.ParseTransferSingle(*l)
+				case abi.Events[constant.EventTransferBatch].ID:
+					event, err = ddc1155.ParseTransferBatch(*l)
+				case abi.Events[constant.EventEnterBlacklist].ID:
+					event, err = ddc1155.ParseEnterBlacklist(*l)
+				case abi.Events[constant.EventExitBlacklist].ID:
+					event, err = ddc1155.ParseExitBlacklist(*l)
+				case abi.Events[constant.EventSetURI].ID:
+					event, err = ddc1155.ParseSetURI(*l)
+				}
+			}
+		}
+		if err != nil {
+			log.Error.Printf("failed to parse event: %v", err.Error())
+			return nil, txReceipt, 0, err
+		}
+		if res, ok := event.(*contracts.DDC721Transfer); ok {
+			ddcId = res.DdcId.Uint64()
+		}
+		events = append(events, event)
+	}
 	return
 }
